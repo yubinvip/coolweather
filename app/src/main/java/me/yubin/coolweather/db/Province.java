e98eb19516a94e68be8a153c8be3ff14807a2694
yubin <yubin@yubin.me>
2018-01-22 17:47:29 +0800

    create database settings

---
+++ app/src/main/java/me/yubin/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package me.yubin.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by yubin on 2018/1/22.
+ */
+
+public class Province extends DataSupport{
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
